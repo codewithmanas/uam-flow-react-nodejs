487dc628051890fb89c7882eae137110ef10823a
Manas Ranjan Adha <manas865873@gmail.com>
2024-08-30 08:44:48 +0530

    add basic uam flow frontend

---
+++ frontend/src/components/RegistrationForm.tsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { useState } from "react";
+
+
+const RegistrationForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+  /** 
+   * Handles the submission of the registration form.
+   * @param {React.FormEvent<HTMLFormElement>} e - The form event.
+   * @returns {Promise<void>} - A promise that resolves when the registration has been processed.
+   */
+  const handleRegister = async (
+    e: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    e.preventDefault();
+    try {
+      await axios.post<{ message: string }>(
+        'http://localhost:5000/api/auth/register',
+        { email, password }
+      );
+      alert('Registration successful');
+    } catch (err) {
+      // console.error(err);
+      console.error(err.response.data.message);
+    }
+  };
+  return (
+    <form onSubmit={handleRegister}>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <button type="submit">Register</button>
+    </form>
+  )
+}
+
+export default RegistrationForm
